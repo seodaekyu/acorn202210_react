3c8e6e6e009b87869668b8585c6df9da56477db8
seodaekyu <grarlove7@naver.com>
2022-12-09 15:28:37 +0900

    App07 예제 추가함

---
+++ src/App07.js
@@ -0,0 +1,27 @@
+// App07.js
+
+// 함수 기반으로 컴포넌트 만드는 방법
+
+
+const App = ()=>{
+    
+    return(
+        <div className="container">
+            <h1>함수 기반 컴포넌트</h1>
+
+        </div>
+    );
+}
+
+export default App;
+
+/*
+    export default()=>{
+        return(
+            <div className="container">
+            <h1>함수 기반 컴포넌트</h1>
+
+        </div>
+        );
+    }
+*/
